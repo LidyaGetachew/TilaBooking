6f42312231f88552f162db26e6a902a7f354ccaf
yared tekleyohannes <yaredte45@gmail.com>
2023-12-14 18:38:48 +0300

    add newsletter to destination page

--- tila-booking/src/pages/Destinations.jsx
+++ tila-booking/src/pages/Destinations.jsx
@@ -2,16 +2,18 @@ import React, { useState } from 'react';
 import "../components/Destinations/destinations.css"
 import SearchBar from '../components/Destinations/search-bar/SearchBar';
 import PopularDestinations from '../components/Destinations/popular-destinations.jsx/PopularDestinations';
+import NewsLetter from '../shared/Newsletter';
 
 const Destinations = () => {
   
 
   return (
-    <div>
+    <div className='destination-wrapper'>
       <div className='search-bar-container'>
         <SearchBar />
       </div>
       <PopularDestinations />
+      <NewsLetter />
     </div>
   );
 };
