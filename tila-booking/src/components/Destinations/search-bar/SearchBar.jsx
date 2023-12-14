d1f717e8973ec8dd3ef9e6d46cc2d5cc4403b47b
yared tekleyohannes <yaredte45@gmail.com>
2023-12-14 15:12:32 +0300

    add focus and blur functionality for the search bar of destination page

--- tila-booking/src/components/Destinations/search-bar/SearchBar.jsx
+++ tila-booking/src/components/Destinations/search-bar/SearchBar.jsx
@@ -1,55 +1,85 @@
-import React,{useState} from 'react'
-import "./searchbar.css"
-import { SearchOutlined } from '@mui/icons-material'
+import React, { useState } from 'react';
+import "./searchbar.css";
+import { SearchOutlined } from '@mui/icons-material';
 import { useNavigate } from "react-router-dom";
 
-
 const SearchBar = () => {
-    const [searchQuery,setSearchQuery] = useState('')
-    const navigate = useNavigate();
+  const [searchQuery, setSearchQuery] = useState('');
+  const [isFocused, setIsFocused] = useState(false);
+  const navigate = useNavigate();
 
-    const destinations = [
-        { id: 1, name: 'Addis Ababa' },
-        { id: 2, name: 'Lalibela' },
-        { id: 3, name: 'Axum' },
-        { id: 4, name: 'Gondar' },
-        { id: 5, name: 'Bahir Dar' },
-        { id: 6, name: 'Harar' },
-        { id: 7, name: 'Danakil Depression' },
-        { id: 8, name: 'Simien Mountains' },
-        { id: 9, name: 'Omo Valley' },
-        { id: 10, name: 'Lake Tana' },
-      ];
+  const destinations = [
+    { id: 1, name: 'Addis Ababa' },
+    { id: 2, name: 'Lalibela' },
+    { id: 3, name: 'Axum' },
+    { id: 4, name: 'Gondar' },
+    { id: 5, name: 'Bahir Dar' },
+    { id: 6, name: 'Harar' },
+    { id: 7, name: 'Danakil Depression' },
+    { id: 8, name: 'Simien Mountains' },
+    { id: 9, name: 'Omo Valley' },
+    { id: 10, name: 'Lake Tana' },
+  ];
 
-      const filteredDestinations = destinations.filter((destination) =>
-        destination.name.toLowerCase().includes(searchQuery.toLowerCase())
-     );
-    return (
-    <>
-    <div className='input-wrapper'>
-        <SearchOutlined />
-        <input 
-        placeholder='Enter Destination...'
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-        />
-    </div>
-    <div className='search-results-list'>
-        {filteredDestinations.map((destination) => (
-        <div className="single-search-result" key={destination.id} onClick={(e) => navigate("/hotel/list",{state:{destination:destination.name,date: [{
-          startDate: new Date(),
-          endDate: new Date(),
-          key: 'selection'
-        }],options: {adult:1,
-          children:0,
-          room:1,
+  const filteredDestinations = destinations.filter((destination) =>
+    destination.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  const handleFocus = () => {
+    setIsFocused(true);
+  };
+
+  const handleBlur = () => {
+    setIsFocused(false);
+  };
+
+  const handleDestinationClick = (destination) => {
+    navigate("/hotel/list", {
+      state: {
+        destination: destination.name,
+        date: [
+          {
+            startDate: new Date(),
+            endDate: new Date(),
+            key: 'selection'
+          }
+        ],
+        options: {
+          adult: 1,
+          children: 0,
+          room: 1,
         }
-      }})
-    }>{destination.name}</div>
-      ))}
-    </div>
-    </>
-  )
-}
+      }
+    });
+  };
 
-export default SearchBar
+  return (
+    <>
+      <div className='input-wrapper'>
+        <SearchOutlined />
+        <input
+          placeholder='Enter Destination...'
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+        />
+      </div>
+      {isFocused && (
+        <div className='search-results-list'>
+          {filteredDestinations.map((destination) => (
+            <div
+              className="single-search-result"
+              key={destination.id}
+              onClick={() => handleDestinationClick(destination)}
+            >
+              {destination.name}
+            </div>
+          ))}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default SearchBar;
