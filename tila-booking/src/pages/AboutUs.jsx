e52c881e848cd071202666aa071140bb3a863966
yared tekleyohannes <yaredte45@gmail.com>
2023-12-21 22:49:31 +0300

    modify  about us page

--- tila-booking/src/pages/AboutUs.jsx
+++ tila-booking/src/pages/AboutUs.jsx
@@ -35,27 +35,6 @@ const AboutUs = () => {
       <section>
         <Team/>
       </section>
-
-      <section>
-        <div>
-          <h2>Testimonials</h2>
-          {/* Testimonials component */}
-        </div>
-      </section>
-
-      <section>
-        <div>
-          <h2>Our Services</h2>
-          {/* Services component */}
-        </div>
-      </section>
-
-      <section>
-        <div>
-          <h2>Our Brand Story</h2>
-          {/* Brand Story component */}
-        </div>
-      </section>
     </div>
   );
 };
